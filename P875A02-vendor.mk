bc0619a08e17032380c41334095e6952d27dce1f
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2020-01-31 11:34:15 -0800

    P875A02: Add UClamp support for lahaina
    Sched Tune is deprecated in 5.4 Kernels and is replaced with
    UClamp. As Android releases has to support earlier Kernel versions
    like 4.14, UClamp isn't made default yet.
    
    Change-Id: If6f4ae0812ae456089273514de58bb0654bb08c7

--- P875A02-vendor.mk
+++ P875A02-vendor.mk
@@ -613,7 +613,6 @@ PRODUCT_COPY_FILES += \
     vendor/zte/P875A02/proprietary/vendor/etc/ssg/ta_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/ssg/ta_config.json \
     vendor/zte/P875A02/proprietary/vendor/etc/ssg/tz_whitelist.json:$(TARGET_COPY_OUT_VENDOR)/etc/ssg/tz_whitelist.json \
     vendor/zte/P875A02/proprietary/vendor/etc/st54j_conf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/st54j_conf.txt \
-    vendor/zte/P875A02/proprietary/vendor/etc/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     vendor/zte/P875A02/proprietary/vendor/etc/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     vendor/zte/P875A02/proprietary/vendor/etc/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/etc/video_system_specs.json \
     vendor/zte/P875A02/proprietary/vendor/etc/vmmgr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/vmmgr.conf \
