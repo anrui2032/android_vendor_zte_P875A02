3e4037d6f9a75520517836aca74dcb182920b23c
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 02:23:32 +0300

    P875A02: fix com.qualcomm.qti.dpm.api@1.0 ELF checks
    Change-Id: I759752ccc133d7047d415eb83c5d3c55820437a4

--- P875A02-vendor.mk
+++ P875A02-vendor.mk
@@ -836,7 +836,7 @@ PRODUCT_PACKAGES += \
     com.qualcomm.mcx.distortionmapper \
     com.qualcomm.mcx.linearmapper \
     com.qualcomm.mcx.policy.mfl \
-    com.qualcomm.qti.dpm.api@1.0 \
+    com.qualcomm.qti.dpm.api@1.0-vendor \
     com.qualcomm.qti.imscmservice@1.0 \
     com.qualcomm.qti.imscmservice@2.0 \
     com.qualcomm.qti.imscmservice@2.1 \
