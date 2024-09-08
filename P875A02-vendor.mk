bad08587b8d0f1c6963bedc5285aa955d86b8db6
dianlujitao <dianlujitao@lineageos.org>
2024-09-08 21:26:39 +0800

    P875A02: remove OMX
     * OMX HW video codecs are no longer used after enabling Codec2
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on Lineage.
    
    Change-Id: I65852151c8fb6387add5fc4402ba3f13c97b5f64

--- P875A02-vendor.mk
+++ P875A02-vendor.mk
@@ -615,14 +615,6 @@ PRODUCT_PACKAGES += \
     vulkan.adreno \
     libC2D2 \
     libCB \
-    libOmxAlacDec \
-    libOmxAmrDec \
-    libOmxAmrwbplusDec \
-    libOmxApeDec \
-    libOmxEvrcDec \
-    libOmxG711Dec \
-    libOmxQcelp13Dec \
-    libOmxWmaDec \
     libOpenCL \
     libVkLayer_q3dtools \
     libacdb-fts \
@@ -720,11 +712,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.memory.pasrmanager@1.1 \
     vendor.qti.qspmhal@1.0 \
     vendor.qti.voiceprint@1.0 \
-    libOmxDsdDec \
     libadm \
     libadpcmdec \
     libdrc \
-    libdsd2pcm \
     libsmwrapper \
     libsurround_3mic_proc \
     com.zte.eeprom.gc5035_sm8350 \
